66997fb85d9fb0876b48acd1c55fe720408f179c
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-29 11:00:50 +0545

    changes the things

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="w-full h-auto p-4 bg-slate-950">
+        This is header section.
+      </div>
+    </>
+  );
+};
+
+export default Header;
