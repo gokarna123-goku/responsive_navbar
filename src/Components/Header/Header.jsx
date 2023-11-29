13f06bf164a6bbd5c61f6adeb68128a989e550f2
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-29 11:06:56 +0545

    changes the things

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <div className="w-full h-auto p-4 bg-slate-950">
-        This is header section.
+        <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+          <div className="flex items-center space-x-4">
+            <div className="text-2xl font-bold">Logo</div>
+            <div className="hidden md:flex space-x-4">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+            </div>
+          </div>
+
+          {/* <div className="md:hidden">
+            {isMenuOpen ? (
+              <FaTimes onClick={toggleMenu} size={24} />
+            ) : (
+              <FaBars onClick={toggleMenu} size={24} />
+            )}
+          </div> */}
+
+          {/* <div
+            className={`md:flex ${
+              isMenuOpen ? "flex flex-col mt-4" : "hidden"
+            }`}
+          >
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+          </div> */}
+
+          <div className="md:flex items-center space-x-4">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded">
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
