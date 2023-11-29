eb3d5bb2d9b7ea5560735e70d84590f8890936e0
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-29 11:14:37 +0545

    working on the navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
+      <Router>
+        <div className="w-full min-h-screen bg-slate-950">
+          <Header />
+          <Home />
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="w-full h-auto bg-slate-900">This is Home Section.</div>
+    </>
+  );
+};
+
+export default Home;
