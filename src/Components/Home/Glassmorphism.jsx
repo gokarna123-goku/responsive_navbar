6feea3ed70e7273d125c2e7c046d524c54e870af
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-30 08:47:53 +0545

    working on the section

--- src/Components/Home/Glassmorphism.jsx
+++ src/Components/Home/Glassmorphism.jsx
@@ -4,8 +4,10 @@ import "./Glassmorphism.css";
 const Glassmorphism = () => {
   return (
     <>
-      <div className="w-full h-auto my-10 py-64 glassmorphism">
-        This is Glassmorphism design.
+      <div className="w-full h-auto my-10 py-64 glassmorphism flex items-center justify-center">
+        <div className="bg-slate-950 rounded-md w-full mx-16 p-40 bg-opacity-60 backdrop-filter backdrop-blur-sm text-4xl text-gray-100 font-black">
+          This is Glassmorphism design.
+        </div>
       </div>
     </>
   );
