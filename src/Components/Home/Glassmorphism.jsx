f34093893f0ae30c3773ff7c7d4f9e3acf4ea051
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-30 08:35:17 +0545

    working on the glassmorphism design

--- src/Components/Home/Glassmorphism.jsx
+++ src/Components/Home/Glassmorphism.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Glassmorphism.css";
 
 const Glassmorphism = () => {
   return (
     <>
-      <div className="w-full h-auto my-10 py-4 bg-slate-700">
+      <div className="w-full h-auto my-10 py-4 bg-slate-700 glassmorphism">
         This is Glassmorphism design.
       </div>
     </>
