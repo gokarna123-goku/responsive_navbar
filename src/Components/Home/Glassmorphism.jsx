69b08b3b8ab2275b787655e76972376324e27b40
Gokarna Adhikari <agokarna444@gmail.com>
2023-11-30 08:37:23 +0545

    changes the things

--- src/Components/Home/Glassmorphism.jsx
+++ src/Components/Home/Glassmorphism.jsx
@@ -4,7 +4,7 @@ import "./Glassmorphism.css";
 const Glassmorphism = () => {
   return (
     <>
-      <div className="w-full h-auto my-10 py-4 bg-slate-700 glassmorphism">
+      <div className="w-full h-auto my-10 py-64 glassmorphism">
         This is Glassmorphism design.
       </div>
     </>
