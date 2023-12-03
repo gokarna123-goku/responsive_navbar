acd30c13320c173b31ecc6a93d141de23c925312
Gokarna Adhikari <agokarna444@gmail.com>
2023-12-03 11:50:51 +0545

    completed the fully responsive navbar section

--- src/Components/Header/Navbar2.jsx
+++ src/Components/Header/Navbar2.jsx
@@ -6,7 +6,7 @@ const Navbar2 = () => {
   const [navbar, setNavbar] = useState(false);
   return (
     <>
-      <nav className="w-full h-auto px-24 py-2 bg-slate-800 shadow-md">
+      <nav className="w-full h-auto lg:px-24 md:px-16 sm:px-14 px-12 py-2 bg-slate-800 shadow-md">
         <div className="justify-between mx-auto lg:w-full md:items-center md:flex">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -15,7 +15,7 @@ const Navbar2 = () => {
               </Link>
               <div className="md:hidden">
                 <button
-                  className="p-2 text-gray-700 rounded-md outline-none border border-slate-800 focus:border-gray-400 focus:border"
+                  className="p-2 text-gray-700 rounded-md outline-none border border-transparent focus:border-gray-400 focus:border"
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
@@ -39,7 +39,7 @@ const Navbar2 = () => {
                 navbar ? "block" : "hidden"
               }`}
             >
-              <ul className="list-none lg:flex md:block sm:block block items-center gap-x-5 gap-y-16">
+              <ul className="list-none lg:flex md:flex sm:block block items-center gap-x-5 gap-y-16">
                 <li>
                   <Link className="text-[1.15rem] font-medium tracking-wider text-gray-200 ease-out duration-700">
                     Home
@@ -65,9 +65,9 @@ const Navbar2 = () => {
                     Contact
                   </Link>
                 </li>
-                <li className="ml-10">
+                <li className="lg:ml-10 md:ml-6 sm:ml-6 ml-3">
                   <button className="bg-orange-500 text-[1.1rem] font-normal text-white px-5 py-1.5 rounded">
-                    Create Account
+                    Register
                   </button>
                 </li>
               </ul>
