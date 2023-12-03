6af91a1ba6be7c6b7ef4b0054ef28ba9c160ad08
Gokarna Adhikari <agokarna444@gmail.com>
2023-12-03 11:35:02 +0545

    styling the navbar section

--- src/Components/Header/Navbar2.jsx
+++ src/Components/Header/Navbar2.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar2 = () => {
-    const [navbar, setNavbar] = useState(false);
+  const [navbar, setNavbar] = useState(false);
   return (
     <>
-      <nav className="w-full bg-white shadow">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+      <nav className="w-full h-auto px-16 py-2 bg-slate-800 shadow-md">
+        <div className="justify-between mx-auto lg:w-full md:items-center md:flex">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              <a href="/)">
-                <h2 className="text-2xl font-bold">LOGO</h2>
-              </a>
+              <Link className="text-3xl text-orange-500 font-semibold tracking-[0.1rem]">
+                Navbar
+              </Link>
               <div className="md:hidden">
                 <button
-                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  className="p-2 text-gray-700 rounded-md outline-none border border-slate-800 focus:border-gray-400 focus:border"
                   onClick={() => setNavbar(!navbar)}
                 >
                   {navbar ? (
@@ -34,22 +35,40 @@ const Navbar2 = () => {
           </div>
           <div>
             <div
-              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={`flex-1 justify-self-center md:block ${
                 navbar ? "block" : "hidden"
               }`}
             >
-              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-                <li className="text-gray-600 hover:text-blue-600">
-                  <a href="/)">Home</a>
+              <ul className="list-none lg:flex md:block sm:block block items-center gap-x-5 gap-y-16">
+                <li>
+                  <Link className="text-[1.15rem] font-medium tracking-wide text-gray-400 hover:text-gray-200 ease-out duration-700">
+                    Home
+                  </Link>
                 </li>
-                <li className="text-gray-600 hover:text-blue-600">
-                  <a href="/)">Blog</a>
+                <li>
+                  <Link className="text-[1.15rem] font-medium tracking-wide text-gray-400 hover:text-gray-200 ease-out duration-700">
+                    About
+                  </Link>
                 </li>
-                <li className="text-gray-600 hover:text-blue-600">
-                  <a href="/)">About US</a>
+                <li>
+                  <Link className="text-[1.15rem] font-medium tracking-wide text-gray-400 hover:text-gray-200 ease-out duration-700">
+                    Services
+                  </Link>
                 </li>
-                <li className="text-gray-600 hover:text-blue-600">
-                  <a href="/)">Contact US</a>
+                <li>
+                  <Link className="text-[1.15rem] font-medium tracking-wide text-gray-400 hover:text-gray-200 ease-out duration-700">
+                    Portfolio
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-[1.15rem] font-medium tracking-wide text-gray-400 hover:text-gray-200 ease-out duration-700">
+                    Contact
+                  </Link>
+                </li>
+                <li className="ml-10">
+                  <button className="bg-blue-500 text-[1.1rem] font-normal text-white px-5 py-1.5 rounded">
+                    Login
+                  </button>
                 </li>
               </ul>
             </div>
